bb660732917e0d42ba216123a7ffb7b9ea1c28d3
fduxiao <9294979+fduxiao@users.noreply.github.com>
2024-10-09 03:58:40 -0400

    separate review and add some basic facts about relation

--- FormalRewrite.lean
+++ FormalRewrite.lean
@@ -1,4 +1,12 @@
 -- This module serves as the root of the `FormalRewrite` library.
 -- Import modules here that should be built as part of the library.
 import FormalRewrite.Basic
+import FormalRewrite.Review
 import FormalRewrite.Relation
+
+
+/-!
+# Contents
+- `FormalRewrite.Review`: A quick review of inductive propositions by examples in `Nat`.
+- `FormalRewrite.Relation`: Relations in type theory and theorems about them.
+-/
