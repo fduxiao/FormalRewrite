bed165f46912f9e4e038e1da51ab6a90a4344b1d
fduxiao <9294979+fduxiao@users.noreply.github.com>
2024-10-14 00:04:05 -0400

    add closures

--- FormalRewrite/Relation.lean
+++ FormalRewrite/Relation.lean
@@ -9,40 +9,75 @@ def Relation (A: Type) := A -> A -> Prop
 class Reflexive (P: Relation A) where
   refl: P a a
 
+def Relation.refl {A: Type} {P: Relation A} [inst: Reflexive P]:
+  forall (a: A), P a a := fun _a => inst.refl
+
+
 class Antisymmetric (P: Relation A) where
   anti: P a b -> P b a -> a = b
 
+def Relation.anti {A: Type} {P: Relation A} [inst: Antisymmetric P]:
+  forall {a b: A}, P a b -> P b a -> a = b :=
+    fun Hab Hba => inst.anti Hab Hba
+
+
 class Transitive (P: Relation A) where
   trans: P a b -> P b c -> P a c
+
+def Relation.trans {A: Type} {P: Relation A} [inst: Transitive P]:
+  forall {a b c: A}, P a b -> P b c -> P a c :=
+    fun Hab Hbc => inst.trans Hab Hbc
+
+
+class Symmetric (P: Relation A) where
+  symm: P a b -> P b a
+
+def Relation.symm {A: Type} {P: Relation A} [inst: Symmetric P]:
+  forall {a b: A}, P a b -> P b a := inst.symm
+
+
+inductive Fold (P: Relation A): Nat -> Relation A where
+  | zero: Fold P 0 a a
+  | succ: Fold P n a b -> P a b -> Fold P (n+1) a b
+
+
+inductive Inverse (P: Relation A): Relation A where
+  | inv: P a b -> Inverse P b a
+
+
+def Relation.inv (P: Relation A): P a b -> Inverse P b a:= Inverse.inv
+
 
 /--
 The defintion of a sub relation
 -/
 class SubRel (P: Relation A) (Q: Relation A): Prop where
-  sub: forall {a b}, P a b -> Q a b
+  inclusion: forall {a b}, P a b -> Q a b
+
+notation: 60 P " sub_rel " Q => SubRel P Q
+
+def Relation.super {A: Type} {P: Relation A} {Q: Relation A}
+  [inst: P sub_rel Q]: forall {a b: A}, P a b -> Q a b :=
+    inst.inclusion
 
 /-!
 `SubRel` is it self a poset on all relations
 -/
 section
 
+instance: SubRel P P where
+  inclusion := id
+
 instance: forall {A: Type}, Reflexive (SubRel (A := A)) where
-  -- refl := (fun {R} => (SubRel.mk (fun {a} {b} H => H)))
-  refl := by
-    intros R
-    apply SubRel.mk
-    intros _a _b H
-    apply H
+  refl {P} := SubRel.mk P.super
 
 
 instance: forall {A: Type}, Transitive (SubRel (A := A)) where
-  trans := by
-    intros P Q R
-    intros s1 s2
+  trans {P Q R} {s1 s2} := by
     apply SubRel.mk
     intros a b H
-    apply s2.sub
-    apply s1.sub
+    apply Q.super
+    apply P.super
     apply H
 
 
@@ -51,8 +86,8 @@ theorem sub_equiv: forall {A: Type} {P Q: Relation A},
   intros A P Q s1 s2
   intros a b
   constructor
-  . apply s1.sub
-  . apply s2.sub
+  . apply s1.inclusion
+  . apply s2.inclusion
 
 
 namespace RelEq
@@ -66,8 +101,8 @@ instance: forall {A: Type}, Antisymmetric (SubRel (A := A)) where
     apply rel_eq.mpr
     intros a b
     apply Iff.intro
-    . apply s1.sub
-    . apply s2.sub
+    . apply s1.inclusion
+    . apply s2.inclusion
 
 
 end RelEq
@@ -75,16 +110,63 @@ end RelEq
 end
 
 
-inductive RefClosure (P: Relation A): Relation A where
-  | refl: RefClosure P a a
-  | sub: P a b -> RefClosure P a b
+inductive ReflClosure (P: Relation A): Relation A where
+  | inclusion: P a b -> ReflClosure P a b
+  | refl: ReflClosure P a a
 
-structure RelationTri {A: Type} (P: Relation A) where
-  a: A
-  b: B
-  H: P a b
+instance: P sub_rel ReflClosure P where
+  inclusion := ReflClosure.inclusion
+
+instance: Reflexive (ReflClosure P) where
+  refl := ReflClosure.refl
+
+instance [P sub_rel R] [Reflexive R]: ReflClosure P sub_rel R where
+  inclusion {a b} Hcl := by
+    cases Hcl
+    case inclusion H =>
+      apply P.super
+      apply H
+    case refl => apply R.refl
 
 
-notation: 60 a " -[ " func " ]> " b => func a b
-#check (1  -[ Nat.le ]> 2)
-#check (Nat.le: Relation Nat)
+inductive TransitiveClosure (P: Relation A): Relation A where
+  | inclusion: P a b -> TransitiveClosure P a b
+  | trans: TransitiveClosure P a b -> TransitiveClosure P b c -> TransitiveClosure P a c
+
+instance: P sub_rel TransitiveClosure P where
+  inclusion := TransitiveClosure.inclusion
+
+instance: Transitive (TransitiveClosure P) where
+  trans := TransitiveClosure.trans
+
+instance [P sub_rel R] [Transitive R]: TransitiveClosure P sub_rel R where
+  inclusion {a b} Hcl := by
+    induction Hcl
+    case inclusion H =>
+      apply P.super
+      apply H
+    case trans Hab Hbc =>
+      apply R.trans
+      apply Hab
+      apply Hbc
+
+
+inductive SymmetricClosure (P: Relation A): Relation A where
+  | inclusion: P a b -> SymmetricClosure P a b
+  | symm: SymmetricClosure P a b -> SymmetricClosure P b a
+
+instance: P sub_rel SymmetricClosure P where
+  inclusion := SymmetricClosure.inclusion
+
+instance: Symmetric (SymmetricClosure P) where
+  symm := SymmetricClosure.symm
+
+instance [P sub_rel R] [Symmetric R]: SymmetricClosure P sub_rel R where
+  inclusion {a b} Hcl := by
+    induction Hcl
+    case inclusion H =>
+      apply P.super
+      apply H
+    case symm Hab =>
+      apply R.symm
+      apply Hab
